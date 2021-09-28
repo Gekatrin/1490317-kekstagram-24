cac9af9a74a79484175b2501044100d3f9d21203
Gekatrin <kmahyoub@mail.ru>
2021-09-28 23:07:21 +0300

    Задание-2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getRandomNumber(min, max) {
+  if (min>=0)
+  {return Math.floor(Math.random() * (max - min)) + min;}
+}
+
+//источник https://schoolsw3.com/js/js_random.php
+function stringLength (theString, maxLength) {
+  const userInput = stringLength.value;
+  if(userInput.length <= maxLength)
+  {return true;}
+}
+
+//источник http://kodesource.top/javascript/form/string-length.php
