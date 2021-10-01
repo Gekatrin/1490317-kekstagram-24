53d53ba5b53be09134cc466f3ab1158a9287b864
Gekatrin <kmahyoub@mail.ru>
2021-10-01 14:26:48 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 function getRandomNumber(min, max) {
-  if (min>=0)
+  if (0<=max && min>=0 && min<max)
   {return Math.floor(Math.random() * (max - min)) + min;}
+  else {
+    return null;
+  }
 }
-
+getRandomNumber(1, 100);
 //источник https://schoolsw3.com/js/js_random.php
-function stringLength (theString, maxLength) {
-  const userInput = stringLength.value;
-  if(userInput.length <= maxLength)
-  {return true;}
+function stringLength (value, maxLength) {
+  if(value.length <= maxLength) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
+stringLength ('Test', 10);
 //источник http://kodesource.top/javascript/form/string-length.php
