4bb90adda7a7a7d57d30d5eaa7991c10a35f936b
Gekatrin <kmahyoub@mail.ru>
2021-10-25 15:36:53 +0300

    Task-4-2

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ stringLength ('Test', 10);
 //источник http://kodesource.top/javascript/form/string-length.php
 
 
-const DESCRIPTION = [
+const description = [
   'пляж',
   'указатель',
   'залив',
@@ -47,7 +47,7 @@ const DESCRIPTION = [
   'сафари',
 ];
 
-const NAMES = [
+const names = [
   'Артем',
   'Маша',
   'Олег',
@@ -55,7 +55,7 @@ const NAMES = [
   'Саша',
 ];
 
-const MESSAGES = [
+const messages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -64,8 +64,8 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const keksObject = (id) => {
-  const randomDescriptionIndex = getRandomNumber(0, DESCRIPTION.length - 1);
+const generatePhoto = (id) => {
+  const randomDescriptionIndex = getRandomNumber(0, description.length - 1);
   const randomLikesCount = getRandomNumber(15, 200);
   const comments = [];
   const randomCommentsNumber = getRandomNumber(1, 5);
@@ -73,13 +73,13 @@ const keksObject = (id) => {
   for (let index = 0; index < randomCommentsNumber; index++) {
     const randomCommentsId = getRandomNumber(0, 200);
     const randomAvatarIndex = getRandomNumber(1, 6);
-    const randomMessagesIndex = getRandomNumber(0, MESSAGES.length - 1);
-    const randomNamesIndex = getRandomNumber(0, NAMES.length - 1);
+    const randomMessagesIndex = getRandomNumber(0, messages.length - 1);
+    const randomNamesIndex = getRandomNumber(0, names.length - 1);
     comments.push({
       id: randomCommentsId,
       avatar: `img/avatar-${randomAvatarIndex}.svg`,
-      message: MESSAGES[randomMessagesIndex],
-      name: NAMES[randomNamesIndex],
+      message: messages[randomMessagesIndex],
+      name: names[randomNamesIndex],
     });
   }
 
@@ -87,7 +87,7 @@ const keksObject = (id) => {
     id: id,
     url: `photos/${id}.jpg`,
     avatar: `img/avatar-${id}.svg`,
-    description: DESCRIPTION[randomDescriptionIndex],
+    description: description[randomDescriptionIndex],
     likes: randomLikesCount,
     comments: comments,
   };
@@ -96,7 +96,7 @@ const keksObject = (id) => {
 function getPhotos() {
   const photos = [];
   for (let index = 1; index <= 25; index++) {
-    const photo = keksObject(index);
+    const photo = generatePhoto(index);
     photos.push(photo);
   }
   return photos;
